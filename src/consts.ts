02ad6d26681dfd012e453988f6ab5ec8893b193c
Виталий <distortreal@gmail.com>
2026-07-04 22:35:29 +0300

    StormSite design + SEO sitemap and meta

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,14 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
 export const SITE_TITLE = 'Storm Cloud Блог';
 export const SITE_DESCRIPTION =
 	'Практичные статьи о VPS, серверах, облаках и инфраструктуре без лишней теории.';
+export const SITE_KEYWORDS =
+	'VPS, аренда сервера, облачный сервер, DevOps, инфраструктура, безопасность сервера, почасовая аренда VPS, Storm Cloud';
 export const SITE_URL = 'https://stormblog.pages.dev';
 export const MAIN_SITE_URL = 'https://stormnetcloud.com/';
 export const INSTAGRAM_URL = 'https://www.instagram.com/stormnet_cloud';
 export const TIKTOK_URL = 'https://www.tiktok.com/@stormnetcloud';
+export const TELEGRAM_URL = 'https://t.me/stormnetofficial';
+export const OG_IMAGE = `${SITE_URL}/favicon.svg`;
